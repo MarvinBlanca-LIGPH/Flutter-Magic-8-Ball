89a85084e580425ee2dfec49d380cf505c44a9a5
MarvinBlanca-LIGPH <76429291+MarvinBlanca-LIGPH@users.noreply.github.com>
2021-06-18 15:42:58 +0800

    Convert to consumer&provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,47 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:provider/provider.dart';
+
 void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.teal.shade900,
-          appBar: AppBar(
-            title: Text('Magic 8 Ball'),
-            backgroundColor: Colors.teal,
+      ChangeNotifierProvider<RandomGenerator>(
+        create: (_) => RandomGenerator(),
+        child: MaterialApp(
+          home: Scaffold(
+            backgroundColor: Colors.teal.shade900,
+            appBar: AppBar(
+              title: Text('Magic 8 Ball'),
+              backgroundColor: Colors.teal,
+            ),
+            body: MagicBall(),
           ),
-          body: MagicBall(),
         ),
       ),
     );
 
-class MagicBall extends StatefulWidget {
-  const MagicBall({Key key}) : super(key: key);
-
-  @override
-  _MagicBallState createState() => _MagicBallState();
-}
-
-class _MagicBallState extends State<MagicBall> {
+class MagicBall extends StatelessWidget {
   int ballNumber = 1;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        child: Image.asset('images/ball$ballNumber.png'),
-        onPressed: () {
-          setState(() {
-            ballNumber = Random().nextInt(5) + 1;
-          });
-        },
-      ),
-    );
+    return Consumer<RandomGenerator>(builder: (context, random, child) {
+      return Center(
+        child: TextButton(
+          child: Image.asset('images/ball${random.ballNumber}.png'),
+          onPressed: () {
+            random.randomNumber();
+          },
+        ),
+      );
+    });
+  }
+}
+
+class RandomGenerator extends ChangeNotifier {
+  int ballNumber = 1;
+
+  void randomNumber() {
+    ballNumber = Random().nextInt(5) + 1;
+    notifyListeners();
   }
 }
